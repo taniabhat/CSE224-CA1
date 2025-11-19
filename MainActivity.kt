42a794d003f47b4a1801082ed9c80fdd26ecabdc
Tania Bhattacharyya <147084504+taniabhat@users.noreply.github.com>
2025-11-19 17:26:28 +0530

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.taniaca1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val btn=findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+            Toast.makeText(this,"Logged out successfully", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
